c63a93844c57af544b104e1d3bdaa5eacde03632
mmsammar <ma2282@njit.edu>
2020-10-03 18:21:56 -0400

    Created script to switch between scenes, added button functionality to move from title scene to intro, tutorial, and level01 scenes.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Debug.Log("yooo");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
